8a52bdac636ac4e53af6bf94fefa84927978ec5b
JulliaFernandes <julliafernandes41@hotmail.com>
2025-06-29 21:44:39 -0300

    Mudanca streamlit

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # ========== CARREGAMENTO DO MODELO ==========
-model = tf.keras.models.load_model("melhor_modelo2.h5")
+model = tf.keras.models.load_model("melhor_modelo2.h5", compile=False)
 
 # ========== TÍTULO E DESCRIÇÃO ==========
 st.markdown("# 🌍 Classificação Binário de Rios por Imagem")
